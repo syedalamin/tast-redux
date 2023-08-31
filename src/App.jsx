0200a57e10a7a272869586628bfef1dd88f0e139
syedalamin <syedalamin2580@gmail.com>
2023-09-01 00:14:22 +0600

    provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
-import { useState } from 'react'
+
 
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+     <Home></Home>
       
     </>
   )
